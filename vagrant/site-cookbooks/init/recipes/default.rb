dad87e84f867db410ce4d85c7324e0b5588325ec
yujikawase <y.kawase@gigei.jp>
2013-10-03 19:01:10 +0900

    phpインストールにremi導入（途中）

--- vagrant/site-cookbooks/init/recipes/default.rb
+++ vagrant/site-cookbooks/init/recipes/default.rb
@@ -14,11 +14,22 @@ service "iptables" do
 	action [:stop, :disable]
 end
 
-%w{php php-common php-mbstring php-xml php-devel php-process php-cli php-pear php-mysql mysql-server}.each do |p|
-    package p do
-        action :install
-    end
+log "Install php"
+#Install php 5.4
+script "install php 5.4" do
+	interpreter "bash"
+	user        "root"
+	code <<-EOH
+	cp -f /vagrant_settings/remi.repo /etc/yum.repos.d/remi.repo
+	yum -y --enablerepo=remi install php php-common php-mbstring php-xml php-devel php-process php-cli php-pear php-mysql
+	EOH
 end
+#%w{php php-common php-mbstring php-xml php-devel php-process php-cli php-pear php-mysql mysql-server}.each do |p|
+#    package p do
+#        action :install
+#    end
+#end
+
 
 script "httpd_config_setting" do
 	interpreter "bash"
