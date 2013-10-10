96fa53067d37874313238345c30e7e9e2c5a352f
yujikawase <y.kawase@gigei.jp>
2013-10-10 16:53:01 +0900

    CakePHPのトップページへ

--- vagrant/site-cookbooks/hello/recipes/default.rb
+++
@@ -1,9 +0,0 @@
-#
-# Cookbook Name:: hello
-# Recipe:: default
-#
-# Copyright (C) 2013 YOUR_NAME
-# 
-# All rights reserved - Do Not Redistribute
-#
-log "Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
--- vagrant/site-cookbooks/init/recipes/default.rb
+++ vagrant/site-cookbooks/init/recipes/default.rb
@@ -14,21 +14,6 @@ service "iptables" do
 	action [:stop, :disable]
 end
 
-log "Install php"
-#Install php 5.4
-script "install php 5.4" do
-	interpreter "bash"
-	user        "root"
-	code <<-EOH
-	cp -f /vagrant_settings/remi.repo /etc/yum.repos.d/remi.repo
-	yum -y --enablerepo=remi install php php-common php-mbstring php-xml php-devel php-process php-cli php-pear php-mysql
-	EOH
-end
-#%w{php php-common php-mbstring php-xml php-devel php-process php-cli php-pear php-mysql mysql-server}.each do |p|
-#    package p do
-#        action :install
-#    end
-#end
 
 
 script "httpd_config_setting" do
@@ -40,3 +25,10 @@ script "httpd_config_setting" do
 	EOH
 end
 
+script "php_setting" do
+	interpreter "bash"
+	user        "root"
+	code <<-EOH
+	cp -a /vagrant_settings/php.ini /etc/php.ini
+	EOH
+end
