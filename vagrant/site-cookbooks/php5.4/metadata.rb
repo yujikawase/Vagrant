8d6d8b5a0a785239b9cba74544dd5ae40d1fbc5f
yujikawase <y.kawase@gigei.jp>
2013-10-15 19:00:01 +0900

    バージョン指定できるように

--- vagrant/site-cookbooks/php5.4/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'php5.4'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures php5.4'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-
--- vagrant/site-cookbooks/php5.5/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'php5.5'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures php5.5'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-
